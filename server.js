a83815512dc0c934c01c0ee1360fe90fb6577768
Alex F <farrisat1@gmail.com>
2019-11-04 09:51:01 -0800

    mongodb table creation and  auth

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+var bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/router");
 const app = express();
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 const PORT = process.env.PORT || 3001;
+
+
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +18,29 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/user");
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+
+});
+
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// Add routes, both API and view
+app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
